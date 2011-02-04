4966d7a50b8cabf5e5c9884835bce491ba30aec7
chadgh <chansen4@gmail.com>
2011-02-04 14:07:43 -0800

    temp fix for crusher

--- bots/crusher.js
+++ bots/crusher.js
@@ -8,8 +8,8 @@ CrusherBot.prototype.setup = function() {
 
 CrusherBot.prototype.run = function() {
 	// get the opponent's information
-	for (i in this.state.bots) {
-		var bot = this.state.bots[i];
+	for (i in bots) {
+		var bot = bots[i];
 		if (bot.name != this.name) {
 			ox = bot.x;
 			oy = bot.y;
