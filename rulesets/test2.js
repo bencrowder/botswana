77b00a35b77721e576ca004e6ff42503b7c24e87
bencrowder <ben.crowder@gmail.com>
2012-12-05 13:22:48 -0700

    Added obstacles back to test2

--- rulesets/test2.js
+++ rulesets/test2.js
@@ -22,7 +22,13 @@ ruleset.properties.weapons.bullet.numAllowed = 3;
 
 ruleset.generateObstacles = function() {
 	// Simpler obstacle generation for now
-	var obstacles = [];
+	var obstacles = [
+		{ "x": 50, "y": 200, "width": 50, "height": 200 },
+		{ "x": 900, "y": 200, "width": 50, "height": 200 },
+		{ "x": 450, "y": 250, "width": 100, "height": 100 },
+		{ "x": 100, "y": 40, "width": 800, "height": 40 },
+		{ "x": 100, "y": 520, "width": 800, "height": 40 }
+	];
 
 	return obstacles;
 };
