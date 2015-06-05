99fc3135f2e793baf70859ffea61a91b7c354c99
bencrowder <ben.crowder@gmail.com>
2015-06-05 10:36:44 -0600

    Removing from localStorage now works

--- js/botswana.js
+++ js/botswana.js
@@ -24,20 +24,68 @@ $(document).ready(function() {
 	});
 
 
+	
+	// Get localStorage store
+	function getLocalStorage(type) {
+		return JSON.parse(localStorage.getItem('botswana_' + type));
+	}
+
+
+	// Add an item to localStorage
+	function addToLocalStorage(value, type) {
+		// Get the localStorage list
+		var customList = getLocalStorage(type);
+
+		// Make sure the list is there
+		if (!customList || !customList.items) {
+			customList = {
+				'items': [],
+			};
+		}
+
+		// And now save it to localStorage
+		customList.items.push(value);
+		localStorage.setItem('botswana_' + type, JSON.stringify(customList));
+	}
+
+	// Remove an item from localStorage
+	function removeFromLocalStorage(value, type) {
+		// Get the localStorage list
+		var customList = getLocalStorage(type);
+
+		// Make sure the list is there
+		var i = customList.items.indexOf(value);
+		if (i != -1) {
+			customList.items.splice(i, 1);
+		}
+
+		// And now save it to localStorage
+		localStorage.setItem('botswana_' + type, JSON.stringify(customList));
+	}
+
+
+	// Add an item to a list
+	function addItemToList(type, value) {
+		var listElement = $("." + type + "s .list");
+
+		var html = "<div class='" + type + " item custom " + (type == 'bot' ? 'selected' : '') + "' data-uri='" + value  + "'>";
+		html += "<span class='delete'>&times;</span>";
+		html += "<div class='name'>" + value + "</div>";
+		html += "</div>";
+
+		listElement.append(html);
+	}
+
+
 	// Load custom bots/rulesets from localStorage
 	function loadLocalStorage(type) {
-		var customList = JSON.parse(localStorage.getItem('botswana_' + type));
+		var customList = getLocalStorage(type);
 		var listElement = $("." + type + "s .list");
 
 		if (customList) {
 			for (var i in customList.items) {
 				var value = customList.items[i];
-
-				var html = "<div class='" + type + " item custom selected' data-uri='" + value  + "'>";
-				html += "<div class='name'>" + value + "</div>";
-				html += "</div>";
-
-				listElement.append(html);
+				addItemToList(type, value);
 			}
 		}
 	}
@@ -53,26 +101,12 @@ $(document).ready(function() {
 		var value = input.val().trim();
 
 		if (value != '') {
-			var html = "<div class='" + type + " item custom selected' data-uri='" + value  + "'>";
-			html += "<div class='name'>" + value + "</div>";
-			html += "</div>";
-			$("." + type + "s .list").append(html);
+			addItemToList(type, value);
 
 			input.val('');
 
 			// Get the localStorage list
-			var customList = JSON.parse(localStorage.getItem('botswana_' + type));
-
-			// Make sure the list is there
-			if (!customList || !customList.items) {
-				customList = {
-					'items': [],
-				};
-			}
-
-			// And now save it to localStorage
-			customList.items.push(value);
-			localStorage.setItem('botswana_' + type, JSON.stringify(customList));
+			addToLocalStorage(value, type);
 		}
 	}
 
@@ -87,19 +121,31 @@ $(document).ready(function() {
 	});
 
 
+	// Deleting bots/rulesets
+	$(".list").on("click", ".item .delete", function() {
+		var item = $(this).parents(".item");
+		var value = item.attr("data-uri");
+		var type = item.hasClass("bot") ? "bot" : "ruleset";
+
+		// Delete it from the DOM
+		item.slideUp(200, function() {
+			// Delete it from localStorage
+			removeFromLocalStorage(value, type);
+		});
+
+		return false;
+	});
+
+
 	// Selection of bots
 	$(".bots .item").on("click", function() {
 		$(this).toggleClass("selected");
-
-		return false;
 	});
 
 	// Selection of ruleset
 	$(".rulesets .item").on("click", function() {
 		$(this).siblings(".item").removeClass("selected");
 		$(this).addClass("selected");
-
-		return false;
 	});
 });
 
@@ -112,3 +158,12 @@ $(document).keydown(function(e) {
 		return false;
 	}
 });
+
+
+// Array Remove - By John Resig (MIT Licensed)
+// http://ejohn.org/blog/javascript-array-remove/
+function removeFromArray(array, from, to) {
+	var rest = array.slice((to || from) + 1 || array.length);
+	array.length = from < 0 ? array.length + from : from;
+	return array.push.apply(array, rest);
+};
