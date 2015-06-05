bf5ec0eac50579b85051b0960e4bd82f402b11f6
bencrowder <ben.crowder@gmail.com>
2015-06-05 14:56:02 -0600

    Styling

--- rulesets/demo1.js
+++ rulesets/demo1.js
@@ -19,11 +19,11 @@ ruleset.properties.weapons.bullet.numAllowed = 3;
 ruleset.generateObstacles = function() {
 	// Simpler obstacle generation for now
 	var obstacles = [
-		{ "x": 50, "y": 200, "width": 50, "height": 200 },
-		{ "x": 900, "y": 200, "width": 50, "height": 200 },
+		{ "x": 150, "y": 200, "width": 50, "height": 500 },
+		{ "x": 1300, "y": 200, "width": 50, "height": 500 },
 		{ "x": 450, "y": 250, "width": 100, "height": 100 },
-		{ "x": 100, "y": 40, "width": 800, "height": 40 },
-		{ "x": 100, "y": 520, "width": 800, "height": 40 }
+		{ "x": 250, "y": 100, "width": 1000, "height": 40 },
+		{ "x": 250, "y": 760, "width": 1000, "height": 40 }
 	];
 
 	return obstacles;
